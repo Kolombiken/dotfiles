e2817c7bef25592f2584f1802538587d61f06a1d
Ida Franceen <ida@kolombiken.com>
2020-08-08 15:31:44 +0200

    Adjusts brew and new-machine-setup

--- setup-a-new-machine.sh
+++ setup-a-new-machine.sh
@@ -88,7 +88,7 @@ fi
 ### homebrew!
 
 # install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # install all the things
 sh brew.sh
